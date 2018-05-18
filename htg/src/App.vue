c093022e80a32a502b86f0d7785c3e26700e103c
nalakagunathilaka <nalakagunathilaka@gmail.com>
2018-05-18 13:24:18 -0700

    2nd

--- htg/src/App.vue
+++ htg/src/App.vue
@@ -1,48 +1,49 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
-      enable-resize-watcher
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          value="true"
-          v-for="(item, i) in items"
-          :key="i"
-        >
-          <v-list-tile-action>
-            <v-icon v-html="item.icon"></v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"></v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
+    <!--<v-navigation-drawer-->
+      <!--persistent-->
+      <!--:mini-variant="miniVariant"-->
+      <!--:clipped="clipped"-->
+      <!--v-model="drawer"-->
+      <!--enable-resize-watcher-->
+      <!--fixed-->
+      <!--app-->
+    <!--&gt;-->
+      <!--<v-list>-->
+        <!--<v-list-tile-->
+          <!--value="true"-->
+          <!--v-for="(item, i) in items"-->
+          <!--:key="i"-->
+        <!--&gt;-->
+          <!--<v-list-tile-action>-->
+            <!--<v-icon v-html="item.icon"></v-icon>-->
+          <!--</v-list-tile-action>-->
+          <!--<v-list-tile-content>-->
+            <!--<v-list-tile-title v-text="item.title"></v-list-tile-title>-->
+          <!--</v-list-tile-content>-->
+        <!--</v-list-tile>-->
+      <!--</v-list>-->
+    <!--</v-navigation-drawer>-->
     <v-toolbar
       app
       :clipped-left="clipped"
     >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
+      <!--<v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>-->
+      <!--<v-btn icon @click.stop="miniVariant = !miniVariant">-->
+        <!--<v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>-->
+      <!--</v-btn>-->
+      <!--<v-btn icon @click.stop="clipped = !clipped">-->
+        <!--<v-icon>web</v-icon>-->
+      <!--</v-btn>-->
+      <!--<v-btn icon @click.stop="fixed = !fixed">-->
+        <!--<v-icon>remove</v-icon>-->
+      <!--</v-btn>-->
+      
       <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <!--<v-spacer></v-spacer>-->
+      <!--<v-btn icon @click.stop="rightDrawer = !rightDrawer">-->
+        <!--<v-icon>menu</v-icon>-->
+      <!--</v-btn>-->
     </v-toolbar>
     <v-content>
       <router-view/>
@@ -64,7 +65,7 @@
       </v-list>
     </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
+      <span>&copy; CSE'15</span>
     </v-footer>
   </v-app>
 </template>
@@ -76,14 +77,14 @@ export default {
       clipped: false,
       drawer: true,
       fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
+      // items: [{
+      //   icon: 'bubble_chart',
+      //   title: 'Inspire'
+      // }],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Hit The Grounds 2018'
     }
   },
   name: 'App'
